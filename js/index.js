d6de191f0c54966f304e292a491a6601b602f34e
Stefan Trenkel <info@stefantrenkel.de>
2014-05-17 10:54:37 +0200

    include jquery and requirejs

---
+++ js/index.js
@@ -0,0 +1,28 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+      jquery: 'libs/jquery'
+    }
+});
+
+require([
+  "jquery"
+], function($) {
+
+  "use strict";
+
+  $(document).ready(function() {
+    $('#toggleHeadline').click(toggleText);
+    $('#toggleIcon').click(toggleText);
+  });
+
+  var toggleText = function() {
+    $('#toggleText').toggleClass('hidden');
+    if($('#toggleText').hasClass('hidden')) {
+      $('.toggleIcon').text('+');
+    } else {
+      $('.toggleIcon').text('-');
+    }
+  };
+
+});

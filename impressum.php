ee4148530daf78898b94fadd44c312558f97d966
Stefan Trenkel <info@stefantrenkel.de>
2014-05-17 20:02:11 +0200

    includes for impressum; textumbrueche impressum

--- impressum.php
+++ impressum.php
@@ -1,27 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>web worker contest</title>
-	<meta name="robots" content="INDEX,FOLLOW">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="css/style.css">
-</head>
+
+<?php
+	include 'php/head.php';
+?>
+
 <body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<header id="topmenu">
-	<nav>
-		<ul>
-			<li class="logo"><div id="logo"><img src="images/logos/wwc_logo.jpg" alt="WWC-Logo" /></div></li>
-			<li><a href="home.html">Home</a></li>
-			<li><a href="guide.html">Guide</a></li>
-			<li><a href="game.html">Game</a></li>
-			<li><a href="upload.html">Upload</a></li>
-			<li><a href="top20.html">Top 20 & Challenger</a></li>
-			<li><a href="winner.html">Winner</a></li>
-			<li><a target="_blank" href="http://webworkercontest.net/blog/">Blog</a></li>
-		</ul>
-	</nav>
-</header>
+
+<?php
+	$headerWinnerIsActive="active";
+	include 'php/header.php';
+?>
+
 <section id="page">
 	<section id="content">
 		<h2>Impressum</h2>
@@ -32,46 +22,43 @@
 				Stefan Trenkel<br>
 				Tannhäuserplatz 3<br>
 				D-28205 Bremen<br>
-				info@webworkercontest.net<br>
-				<br>
-				<br>
-				Haftungsausschluss<br>
-				<br>
-				Der Autor übernimmt keinerlei Gewähr für die Aktualität, Richtigkeit und Vollständigkeit der bereitgestellten Informationen und Programme auf seiner Website. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen / Programmen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen / Programme verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt. Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.<br>
-				<br>
-				Bei direkten oder indirekten Verweisen auf fremde Webseiten (“Hyperlinks”), die außerhalb des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten /verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen anderen Formen von Datenbanken, auf deren Inhalt externe Schreibzugriffe möglich sind. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.<br>
-				<br>
-				Die vom Author erstellte Programme unterliegen der <a target="_blank" href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>. Die Copyright-Bestimmungen der Web Worker Programme liegen in der Verantwortung der jeweiligen Autoren.<br>
-				<br>
-				Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.<br>
-				<br>
+				info@webworkercontest.net
+				<br><br><br>
+				<u>Haftungsausschluss</u>
+				<br><br>
+				Der Autor übernimmt keinerlei Gewähr für die Aktualität, Richtigkeit und Vollständigkeit der bereitgestellten Informationen und Programme
+				auf seiner Website. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die
+				Nutzung oder Nichtnutzung der dargebotenen Informationen / Programmen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen
+				/ Programme verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein nachweislich vorsätzliches oder grob fahrlässiges
+				Verschulden vorliegt. Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten
+				oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.
+				<br><br>
+				Bei direkten oder indirekten Verweisen auf fremde Webseiten (“Hyperlinks”), die außerhalb des Verantwortungsbereiches des Autors liegen, würde
+				eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch
+				möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor erklärt hiermit ausdrücklich, dass zum
+				Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung,
+				die Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb distanziert er sich hiermit
+				ausdrücklich von allen Inhalten aller verlinkten /verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt
+				für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern,
+				Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen anderen Formen von Datenbanken, auf deren Inhalt externe Schreibzugriffe
+				möglich sind. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung
+				solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige,
+				der über Links auf die jeweilige Veröffentlichung lediglich verweist.
+				<br><br>
+				Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile
+				oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten,
+				bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.
+				<br><br><br>
+				<u>Lizenz</u>
+				<br><br>
+				Die vom Author erstellte Programme unterliegen der <a target="_blank" href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.
+				Die Copyright-Bestimmungen der Web Worker Programme liegen in der Verantwortung der jeweiligen Autoren.
 			</p>
 		</article>
 	</section>
 </section>
-<footer id="bottom">
-	<section id="sponsors">
-		<p class="prizeBy">sponsored by</p>
-		<ul>
-			<li><a target="_blank" href="http://www.team-neusta.de/"><div class="sponsorLogoNeusta sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.dpunkt.de/"><div class="sponsorLogoDpunkt sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.oreilly.de/"><div class="sponsorLogoOreilly sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.heise.de/ix/"><div class="sponsorLogoIX sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.galileocomputing.de/"><div class="sponsorLogoGalileo sponsorLogo"></div></a></li>
-		</ul>
-	</section>
-	<section id="footerLine">
-		<section class="left">
-			<p>Copyright Web Worker Contest 2014</p>
-		</section>
-		<section class="right">
-			<ul>
-				<li><a href="kontakt.html">Kontakt</a></li>
-				<li><a href="impressum.html">Impressum</a></li>
-				<li><a target="_blank" href="https://twitter.com/workercontest"><img src="images/logos/twitter.jpg" alt="WWC-Logo" /></a></li>
-			</ul>
-		</section>
-	</section>
-</footer>
+
+<?php	include 'php/footer.php'; ?>
+
 </body>
 </html>

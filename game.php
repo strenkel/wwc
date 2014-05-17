0312cba5813b1cb5bfb05b118c5146d7d4384e34
Stefan Trenkel <info@stefantrenkel.de>
2014-05-17 17:06:56 +0200

    rename game to php; fix css

--- game.php
+++ game.php
@@ -1,31 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>web worker contest</title>
-	<meta name="robots" content="INDEX,FOLLOW">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="css/style.css">
-</head>
+
+<?php
+	include 'php/head.php';
+?>
+
 <body>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<header id="topmenu">
-	<nav>
-		<ul>
-			<li class="logo"><div id="logo"><img src="images/logos/wwc_logo.jpg" alt="WWC-Logo" /></div></li>
-			<li><a href="home.html">Home</a></li>
-			<li><a href="guide.html">Guide</a></li>
-			<li><a href="game.html" class="active">Game</a></li>
-			<li><a href="upload.html">Upload</a></li>
-			<li><a href="top20.html">Top 20 & Challenger</a></li>
-			<li><a href="winner.html">Winner</a></li>
-			<li><a target="_blank" href="http://webworkercontest.net/blog/">Blog</a></li>
-		</ul>
-	</nav>
-</header>
+
+<?php
+	$headerGameIsActive="active";
+	include 'php/header.php';
+?>
+
 <section id="page" class="gamePage">
 	<section id="content">
 		<div id="game">
-			<h2>WEB WORKER CONTEST</h2>
+			<h2>GAME</h2>
 			<hr width="100">
 			<div id="left40">
 				<canvas id="wwc-canvas" width="500" height="500"></canvas>
@@ -115,29 +105,8 @@
 		</div>
 	</section>
 </section>
-<footer id="bottom">
-	<section id="sponsors">
-		<p class="prizeBy">sponsored by</p>
-		<ul>
-			<li><a target="_blank" href="http://www.team-neusta.de/"><div class="sponsorLogoNeusta sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.dpunkt.de/"><div class="sponsorLogoDpunkt sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.oreilly.de/"><div class="sponsorLogoOreilly sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.heise.de/ix/"><div class="sponsorLogoIX sponsorLogo"></div></a></li>
-			<li><a target="_blank" href="http://www.galileocomputing.de/"><div class="sponsorLogoGalileo sponsorLogo"></div></a></li>
-		</ul>
-	</section>
-	<section id="footerLine">
-		<section class="left">
-			<p>Copyright Web Worker Contest 2014</p>
-		</section>
-		<section class="right">
-			<ul>
-				<li><a href="kontakt.html">Kontakt</a></li>
-				<li><a href="impressum.html">Impressum</a></li>
-				<li><a target="_blank" href="https://twitter.com/workercontest"><img src="images/logos/twitter.jpg" alt="WWC-Logo" /></a></li>
-			</ul>
-		</section>
-	</section>
-</footer>
+
+<?php	include 'php/footer.php'; ?>
+
 </body>
 </html>

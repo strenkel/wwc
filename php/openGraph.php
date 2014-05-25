8561d13610d9a72847141783612a9acbbd3128f9
Stefan Trenkel <info@stefantrenkel.de>
2014-05-25 19:04:36 +0200

    fix opengrapg image

--- php/openGraph.php
+++ php/openGraph.php
@@ -1,5 +1,7 @@
 <meta property="og:title" content="WEB WORKER CONTEST – Ein JavaScript Programmierwettbewerb" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="http://www.webworkercontest.net" />
-<meta property="og:image" content="images/facebook.jpg" />
+<meta property="og:image" content="http://www.webworkercontest.net/images/facebook.jpg" />
+<meta property="og:image:width" content="754" />
+<meta property="og:image:height" content="392" />
 <meta property="og:description" content="Programmierst Du den besten Bot?" />

f0a3de7602c861b6c5f7fdce993fd667f46fabff
Stefan Trenkel <info@stefantrenkel.de>
2014-07-24 16:32:45 +0200

    small refactoring

--- js/game/DrawingArea.js
+++ js/game/DrawingArea.js
@@ -1,15 +1,15 @@
 define(function () {
 
   "use strict";
-  
+
   var DrawingArea = function(c) {
-    
+
     var canvas = c.canvas;
     var unit = c.unit;
-    var context = c.canvas.getContext("2d");
+    var context = canvas.getContext("2d");
     canvas.width = unit * c.width; // x direction
     canvas.height = unit * c.height; // y direction
-    
+
     /**
      * @param position {Position}
      * @param color {css color string} e.g. '#0f0'
@@ -18,12 +18,12 @@ define(function () {
       context.fillStyle = color;
       context.fillRect(position.x * unit, position.y * unit, unit, unit);
     };
-    
+
     this.clear = function() {
       context.clearRect(0, 0, canvas.width, canvas.height);
     };
-    
+
   };
-  
+
   return DrawingArea;
 });

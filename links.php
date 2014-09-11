0656e92abe5569d24632db13017c57db1a23cded
Stefan Trenkel <info@stefantrenkel.de>
2014-09-11 23:06:43 +0200

    add 2 links

--- links.php
+++ links.php
@@ -12,23 +12,25 @@
 ?>
 
 <section id="page">
-	<section id="content">
-		<h2>Links</h2>
-		<hr width="100">
-		<article>
-			<p>
-				<a href='https://hacks.mozilla.org/2014/08/programming-games-in-the-browser/'>Programming games in the browser @ Mozilla Hacks</a><br>
-        <a href='http://blog.team-neusta.de/index.php/spektrum/agile-und-development/programmierspiele-im-browser-html5-machts-moeglich/'>Programmierspiele im Browser - HTML5 macht’s möglich @ team neusta blog</a><br>
+  <section id="content">
+    <h2>Links</h2>
+    <hr width="100">
+    <article>
+      <p>
+        <a href='https://hacks.mozilla.org/2014/08/programming-games-in-the-browser/'>Programming games in the browser</a> | Mozilla Hacks<br>
+        <a href='http://blog.team-neusta.de/index.php/spektrum/agile-und-development/programmierspiele-im-browser-html5-machts-moeglich/'>Programmierspiele im Browser - HTML5 macht’s möglich</a> | team neusta blog<br>
         <br>
-        <a href='http://www.heise.de/developer/meldung/Sieger-des-Web-Worker-Contest-stehen-fest-2237774.html'>Sieger des Web Worker Contest stehen fest @ heise developer</a><br>
-        <a href='http://www.heise.de/developer/meldung/Sieger-der-ersten-Runde-des-Web-Worker-Contest-stehen-fest-2218120.html'>Sieger der ersten Runde des Web Worker Contest stehen fest @ heise developer</a><br>
-        <a href='http://www.heise.de/developer/meldung/Web-Worker-Contest-Browser-Programmierwettbewerb-gestartet-2197426.html'>Web Worker Contest: Browser-Programmierwettbewerb gestartet @ heise developer</a><br>
-        <a href='http://www.itespresso.de/2014/05/23/entwickler-startet-javascript-programmierspiel/'>Entwickler startet JavaScript-Programmierspiel @ ITespresso</a><br>
-        <a href='http://www.it-daily.net/it-strategie/aktuelles/9144-web-worker-contest'>Web Worker Contest @ itdaily</a><br>
-        <a href='http://www.mathematik.de/ger/diverses/aktuelles/wwc_programmieren_spielen_und_gewinnen.html'>WWC: Programmieren, spielen und gewinnen @ Mathematik.de</a><br>
+        <a href='http://dailyjs.com/2014/06/13/web-worker-steady-jasmine-int/'>Web Worker Contest, Steady.js, Jasmine Integration Tests</a> | DailyJS<br>
+        <a href='http://www.heise.de/developer/meldung/Sieger-des-Web-Worker-Contest-stehen-fest-2237774.html'>Sieger des Web Worker Contest stehen fest</a> | heise developer<br>
+        <a href='http://www.heise.de/developer/meldung/Sieger-der-ersten-Runde-des-Web-Worker-Contest-stehen-fest-2218120.html'>Sieger der ersten Runde des Web Worker Contest stehen fest</a> | heise developer<br>
+        <a href='http://www.heise.de/developer/meldung/Web-Worker-Contest-Browser-Programmierwettbewerb-gestartet-2197426.html'>Web Worker Contest: Browser-Programmierwettbewerb gestartet</a> | heise developer<br>
+        <a href='http://www.itespresso.de/2014/05/23/entwickler-startet-javascript-programmierspiel/'>Entwickler startet JavaScript-Programmierspiel</a> | ITespresso<br>
+        <a href='http://www.it-daily.net/it-strategie/aktuelles/9144-web-worker-contest'>Web Worker Contest</a> | itdaily<br>
+        <a href='http://dmv.mathematik.de/index.php/forum/nachrichten/184-mit-blindstrategien-auf-zum-webworker-contest'>Mit Blindstrategien auf zum Webworker-Contest</a> |  Deutsche Mathematiker-Vereinigung<br>
+        <a href='http://www.mathematik.de/ger/diverses/aktuelles/wwc_programmieren_spielen_und_gewinnen.html'>WWC: Programmieren, spielen und gewinnen</a> | Mathematik.de<br>
         <br>
-        <a href='https://www.youtube.com/channel/UCNqOUOlC8UOYYLdGHhFbY-Q'>Videos @ YouTube</a><br>
-        <a href='https://github.com/strenkel/wwc'>Code @ GitHub</a><br>
+        <a href='https://www.youtube.com/channel/UCNqOUOlC8UOYYLdGHhFbY-Q'>Videos</a>| YouTube<br>
+        <a href='https://github.com/strenkel/wwc'>Code</a> |GitHub<br>
       </p>
 		</article>
 	</section>
